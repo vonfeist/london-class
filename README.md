97b3fff49d949a2ddfec80729f82bf33f80e32f3
Peter Kassenaar <info@kassenaar.com>
2017-11-28 11:04:49 +0000

    Added first code example (Video Manager)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # london-class
-Slides and additional links on ABC class London, Nov. 2017
+Slides and additional links on ABC class London, Nov. 2017.
+- go to /slides for PDF versions of the slides on the start of each day.
+- see /code for example code we were building (the Video Manager App).
+	- DO execute `npm install` and then `npm start` to start the example application. 
 
 ## Links from the training
-- ....TODO....
+- Difference between [innerText] and [innerHtml]: https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
+- ...
---
+++ code/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,26 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {VideoDashboardComponent} from './video-dashboard/video-dashboard.component';
+import {VideoListComponent} from './video-list/video-list.component';
+import {VideoPlayerComponent} from './video-player/video-player.component';
+import {StatFiltersComponent} from './stat-filters/stat-filters.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+	{path: '',  component: VideoDashboardComponent}
+]
+
+@NgModule({
+	imports     : [
+		CommonModule,
+		RouterModule.forChild(routes)
+	],
+	declarations: [
+		VideoDashboardComponent,
+		VideoListComponent,
+		VideoPlayerComponent,
+		StatFiltersComponent
+	]
+})
+export class DashboardModule {
+}
